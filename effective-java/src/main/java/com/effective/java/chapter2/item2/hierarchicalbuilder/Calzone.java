3264e29c8d687cf7aa4107f4fc9c2512bd275126
hisler <habiphakan.isler@gmail.com>
2019-08-20 12:18:38 +0300

    hierarchical builder implemented

---
+++ effective-java/src/main/java/com/effective/java/chapter2/item2/hierarchicalbuilder/Calzone.java
@@ -0,0 +1,38 @@
+package com.effective.java.chapter2.item2.hierarchicalbuilder;
+
+public class Calzone extends Pizza {
+
+    private final boolean sauceInside;
+
+    public static class Builder extends Pizza.Builder<Builder> {
+        private boolean sauceInside = false;//Default value
+
+        public Builder sauceInside() {
+            sauceInside = true;
+            return this;
+        }
+
+        @Override
+        public Calzone build() {
+            return new Calzone(this);
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+
+
+    }
+
+
+    private Calzone(Builder builder) {
+        super(builder);
+        this.sauceInside = builder.sauceInside;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Calzone with %s and sauce on the %s", toppings, sauceInside ? "inside" : "outside");
+    }
+}
---
+++ effective-java/src/main/java/com/effective/java/chapter2/item2/hierarchicalbuilder/Pizza.java
@@ -0,0 +1,28 @@
+package com.effective.java.chapter2.item2.hierarchicalbuilder;
+import java.util.*;
+
+
+
+// Note that the underlying "simulated self-type" idiom  allows for arbitrary fluid hierarchies, not just builders
+
+public abstract class Pizza {
+    public enum Topping { HAM, MUSHROOM, ONION, PEPPER, SAUSAGE }
+    final Set<Topping> toppings;
+
+    abstract static class Builder<T extends Builder<T>> {
+        EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+        public T addTopping(Topping topping) {
+            toppings.add(Objects.requireNonNull(topping));
+            return self();
+        }
+
+        abstract Pizza build();
+
+        // Subclasses must override this method to return "this"
+        protected abstract T self();
+    }
+    
+    Pizza(Builder<?> builder) {
+        toppings = builder.toppings.clone();
+    }
+}
